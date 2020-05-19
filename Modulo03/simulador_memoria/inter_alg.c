7f9af315d9543425e1373365f60f7419107720e4
RafaelSantosBraz <rafaelsantosbrazpfi@gmail.com>
2020-05-19 17:55:30 -0300

    main functions as an interface

--- Modulo03/simulador_memoria/inter_alg.c
+++ Modulo03/simulador_memoria/inter_alg.c
@@ -28,3 +28,55 @@ PAGE *remove_page(PAGE *page)
         break;
     }
 }
+
+PAGE *insert_set_of_pages(PAGE *pages_set, int size)
+{
+    switch (CURRENT_METHOD)
+    {
+    case LRU:
+        // return função_implementada_em_LRU(pages_set, size);
+        break;
+    case CLOCK:
+        // return função_implementada_em_CLOCK(pages_set, size);
+        break;
+    }
+}
+
+PAGE *insert_page(PAGE *page)
+{
+    switch (CURRENT_METHOD)
+    {
+    case LRU:
+        // return função_implementada_em_LRU(page);
+        break;
+    case CLOCK:
+        // return função_implementada_em_CLOCK(page);
+        break;
+    }
+}
+
+void initialize_list()
+{
+    switch (CURRENT_METHOD)
+    {
+    case LRU:
+        // função_implementada_em_LRU();
+        break;
+    case CLOCK:
+        // função_implementada_em_CLOCK();
+        break;
+    }
+}
+
+int *remove_best_page()
+{
+    switch (CURRENT_METHOD)
+    {
+    case LRU:
+        // return função_implementada_em_LRU();
+        break;
+    case CLOCK:
+        // return função_implementada_em_CLOCK();
+        break;
+    }
+}
--- Modulo03/simulador_memoria/inter_alg.h
+++ Modulo03/simulador_memoria/inter_alg.h
@@ -22,4 +22,34 @@ PAGE *remove_set_of_pages(PAGE *, int);
  * Se NULL for retornado, não foi possível remover a página.
  */
 PAGE *remove_page(PAGE *);
+/**
+ * insere na lista global de páginas ativas as páginas informadas pelo vetor de páginas.
+ * O último parâmetro indica o tamanho do vetor de páginas informado.
+ * Se tudo der certo, o ponteiro para o vetor de páginas informado será retornado (!= NULL).
+ * Se NULL for retornado, não foi possível inserir alguma(s) das páginas.
+ */
+PAGE *insert_set_of_pages(PAGE *, int);
+/**
+ * insere na lista global de páginas ativas a página informada.
+ * Se tudo der certo, o ponteiro para a página informada será retornado (!= NULL).
+ * Se NULL for retornado, não foi possível inserir a página.
+ */
+PAGE *insert_page(PAGE *);
+/**
+ * inicializa a lista global de páginas ativas.
+ */
+void initialize_list(void);
+/**
+ * Seção principal dos algortimos de substituição de páginas.
+ * Esta função é acionada quando ocorrer uma falta de página e não existirem quadros de páginas livres.
+ * A ideia dessa função é selecionar a melhor páginas (conforme o algoritmo em execução) para ser substituída.
+ * O retorno é o número do quadro de página que foi liberado ao desmapear alguma página virtual ativa.
+ * -- Esta função já implica na remoção da página desmapeada da lista global e a marcação do bit 'present'
+ * da página desmapeada como NOT_PRESENT.
+ * Se tudo der certo, o vetor de bits que representa o número do quadro de página liberado é retornado.
+ * O tamanho do vetor de bits é constante e representado por FRAME_NUMBER_LEN.
+ * Se NULL for retornado, não foi possível desmapear alguma página e o erro deve ser tratado.
+ */
+int *remove_best_page(void);
+
 #endif
